3302e07d5317be09605d3731a7a2f950f38d0362
LeineLab Kasse <kasse@leinelab.org>
2022-10-13 20:51:31 +0200

    Initial commit

---
+++ settings.py
@@ -0,0 +1,12 @@
+
+# Database
+db_host = 'localhost'
+db_user = 'nfc_kasse'
+db_password = 'password'
+db_database = 'nfc_kasse'
+
+# Barcodescanner
+serialport = '/dev/ttyS0'
+
+# Optional: domain to use for OTP
+#domain = 'kasse.hacker.space'
