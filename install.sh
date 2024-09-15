8ad58498fbcdd349bdf5f7d02ec4cfd6ceb5f2e0
LeineLab Kasse <kasse@leinelab.org>
2024-09-15 21:47:33 +0200

    Updated install script

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
-
-cp ./ /opt/kasse
+#!/bin/bash
+if [ !-f settings.py ]
+then
+	echo "Please make sure to setup the database and settings.py before running this script."
+	exit 1
+fi
+cp -r ./ /opt/kasse
 cp nfckasse.service /etc/systemd/system/
-systremctl enable nfckasse.service
+systemctl enable nfckasse.service
 cd /opt/kasse
-pip3 install -r requirements.txt
+python -m venv venv
+venv/bin/pip install -r requirements.txt
 
-systremctl start nfckasse.service
+systemctl start nfckasse.service
 
 #optional
 sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80 -j REDIRECT --to-port 5000
