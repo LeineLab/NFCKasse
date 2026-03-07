cb155fd77fe8574e4ee594779d291373a0d96857
Luca Schröder <git@sndstrm.de>
2026-03-07 18:58:40 +0100

    Clean up button mess

--- kasse.py
+++ kasse.py
@@ -21,6 +21,9 @@ _lang_index = 0
 i18n.load(_languages)
 i18n.set_language(_languages[0])
 
+RIGHT_BUTTON = 1
+LEFT_BUTTON  = 0
+NO_BUTTON    = -1
 
 def _lang_btn():
 	"""Return the display name of the next language, or '' if only one language is configured."""
@@ -35,16 +38,16 @@ def _cycle_language():
 	i18n.set_language(_languages[_lang_index])
 
 
-def buttonLoop(timeout : int = 20, countdown_button : int = -1):
+def buttonLoop(timeout : int = 20, countdown_button : int = NO_BUTTON):
 	tout = time.time() + timeout
 	lsecs = -1
 	btns.resetState()
 	while tout > time.time():
 		pressed = btns.getPressed()
-		if pressed[1]: #cancel
-			return 0
-		if pressed[0]: #ok
-			return 1
+		if pressed[RIGHT_BUTTON]: #cancel
+			return RIGHT_BUTTON
+		if pressed[LEFT_BUTTON]: #ok
+			return LEFT_BUTTON
 		if countdown_button != -1:
 			csecs = int(tout - time.time())
 			if csecs != lsecs:
@@ -52,16 +55,18 @@ def buttonLoop(timeout : int = 20, countdown_button : int = -1):
 				lsecs = csecs
 		time.sleep(0.1)
 	logging.info("Waiting for buttons timed out")
-	return -1
+	return countdown_button
 
 def showConnectUri(uid : int):
 	url = api.getConnectLink(uid)
 	if url:
 		disp.showQR(url)
+		# Allow for any button press to quit
 		buttonLoop()
 	else:
 		disp.error(_('msg.already_connected'))
-		time.sleep(5)
+		# Allow for any button press to quit
+		buttonLoop(5)
 
 def createAccount(uid : int):
 	disp.info(_('msg.card_unknown'), _('btn.register'), _('btn.cancel'))
@@ -79,12 +84,14 @@ def createAccount(uid : int):
 				return True
 			else:
 				disp.error(_('msg.register_failed'))
-				time.sleep(5)
+				# Allow for any button press to quit
+				buttonLoop(5)
 				return False
 		else:
 			# Do not register if card id is not consistent.
 			disp.error(_('msg.card_id_mismatch'))
-			time.sleep(5)
+			# Allow for any button press to quit
+			buttonLoop(5)
 			return False
 	else:
 		return False
@@ -94,11 +101,11 @@ def buyProduct(uid : int, value : float, product):
 	pname = product['name'].replace(' ', '\u00a0')
 	if product['price'] > value:
 		buzz.abort()
-		disp.error(_('msg.product_detail', name=pname, price=product['price'], balance=value), _('btn.scan'), _('btn.cancel'))
+		disp.error(_('msg.product_detail', name=pname, price=product['price'], balance=value), _('btn.scan'), _('btn.logout'))
+		return buttonLoop() == LEFT_BUTTON
 	else:
 		disp.message(_('msg.product_detail', name=pname, price=product['price'], balance=value), _('btn.buy'), _('btn.cancel'))
-	ret = buttonLoop(5, 1)
-	if ret == 1 or ret == -1:
+	if buttonLoop(5, 1) == LEFT_BUTTON:
 		if product['price'] <= value:
 			if api.buyProduct(uid, product['ean']):
 				value, oidc = api.getCard(uid)
@@ -109,7 +116,7 @@ def buyProduct(uid : int, value : float, product):
 				led.red()
 				disp.error(_('msg.purchase_failed'), _('btn.scan'), _('btn.logout'))
 				buzz.abort()
-			if buttonLoop() != 1:
+			if buttonLoop() != LEFT_BUTTON:
 				return False
 	else:
 		if product['price'] > value:
@@ -118,7 +125,7 @@ def buyProduct(uid : int, value : float, product):
 			led.red()
 			value, oidc = api.getCard(uid)
 			disp.error(_('msg.purchase_cancel', balance=value), _('btn.scan'), _('btn.logout'))
-			if not buttonLoop():
+			if buttonLoop() != LEFT_BUTTON:
 				led.red()
 				time.sleep(1)
 				return False
@@ -135,9 +142,9 @@ def ui():
 			disp.dim(20)
 		if uid is None:
 			pressed = btns.getPressed()
-			if pressed[0]:
+			if pressed[LEFT_BUTTON]:
 				uid = settings.uid_guest
-			elif pressed[1]:
+			elif pressed[RIGHT_BUTTON]:
 				_cycle_language()
 				return
 	print(uid)
@@ -145,7 +152,8 @@ def ui():
 	if not api.ping():
 		disp.error(_('msg.no_connection'))
 		led.red()
-		time.sleep(5)
+		# Allow for any button press to quit
+		buttonLoop(5)
 		return
 
 	cancel = 0
@@ -168,13 +176,13 @@ def ui():
 			bc = scan.scan(scan_iteration)
 			scan_iteration += 1
 			pressed = btns.getPressed()
-			if pressed[0] and not oidc and uid != settings.uid_guest:
+			if pressed[LEFT_BUTTON] and not oidc and uid != settings.uid_guest:
 				scan.endScan()
 				# Show barcode
 				showConnectUri(uid)
 				# Quit session
 				return
-			if pressed[1]: # Logout
+			if pressed[RIGHT_BUTTON]: # Logout
 				scan.endScan()
 				return
 		scan.endScan()
@@ -183,7 +191,7 @@ def ui():
 		if bc == None:
 			disp.error(_('msg.no_barcode'), _('btn.scan'), _('btn.logout'))
 			led.blue()
-			if buttonLoop() != 1:
+			if buttonLoop() != LEFT_BUTTON:
 				return
 		else:
 			bc = api.getAlias(bc)
@@ -194,7 +202,7 @@ def ui():
 			if product is None:
 				led.purple()
 				disp.error(_('msg.product_not_found'), _('btn.scan'), _('btn.logout'))
-				if buttonLoop(10, 0) != 1:
+				if buttonLoop(10, 0) != LEFT_BUTTON:
 					return
 				'''
 				# Currently not in use, as with the switch from database to MakerSpaceAPI there is currently no topup code
@@ -202,31 +210,31 @@ def ui():
 				if topupval == None:
 					led.purple()
 					disp.error(_('msg.product_not_found'), _('btn.scan'), _('btn.logout'))
-					if buttonLoop(10, 0) != 1:
-						cancel = 1
+					if buttonLoop(10, 0) != LEFT_BUTTON:
+						return
 				elif isused:
 					led.red()
 					disp.error(_('msg.topup_used'), _('btn.scan'), _('btn.logout'))
-					if buttonLoop(10) != 1:
-						cancel = 1
+					if buttonLoop(10) != LEFT_BUTTON:
+						return
 				else:
 					led.yellow()
 					disp.message(_('msg.topup_value', value=topupval), _('btn.topup'), _('btn.cancel'))
 					b =  buttonLoop(10)
-					if b == 0:
+					if b == RIGHT_BUTTON:
 						disp.success(_('msg.more_items', balance=value), _('btn.scan'), _('btn.logout'))
-						if buttonLoop(10, 0) != 1:
-							cancel = 1
-					elif b == -1:
-						cancel = 1
+						if buttonLoop(10, 0) != LEFT_BUTTON:
+							return
+					elif b == NO_BUTTON:
+						return
 					else:
 						if api.topUpCard(uid, bc):
 							led.green()
 							value, oidc = api.getCard(uid)
 							disp.message(_('msg.hello_balance', balance=value), _('btn.scan'), _('btn.logout'))
 							led.blue()
-							if buttonLoop(10) != 1:
-								cancel = 1
+							if buttonLoop(10) != LEFT_BUTTON:
+								return
 						else:
 							led.red()
 						time.sleep(1)
